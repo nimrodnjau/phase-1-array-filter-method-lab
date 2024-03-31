a1f87ef5846d637267da19a6f6db46101bb17be1
nimrod njau <nimrodnjau@gmail.com>
2024-03-31 22:42:00 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+let word = "Sammy"
+function findMatching(drivers, word ) {
+    return drivers.filter(
+        drivers => drivers.toLowerCase() === word.toLowerCase()
+    )
+}
+console.log(findMatching(drivers, word))
+
+
+
+
+let test = "A"
+function fuzzyMatch(drivers, test) {
+    return drivers.filter(
+        drivers => drivers.toLowerCase() .indexOf(test.toLowerCase()) === 0
+    )
+}
+console.log(fuzzyMatch(drivers, test))
+
+
+
+
+
+const drivers2 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  let nameSearch = "Annette"
+  function matchName(drivers2, nameSearch) {
+    return drivers2.filter(drivers2 => drivers2.name === nameSearch)
+  }
+  console.log(matchName(drivers2, nameSearch))
